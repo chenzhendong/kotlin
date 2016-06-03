7e285c558d2c07f36c6caed9295ee8cd8bfe25ef
Zhendong Chen <chenzhendong@yahoo.com>
2016-06-02 20:48:49 -0400

    init hello world

---
+++ vertx-web/src/main/kotlin/Hello.kt
@@ -0,0 +1,6 @@
+package com.popyoyo.kotlin.ktweb
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ vertx-web/src/test/kotlin/HelloTest.kt
@@ -0,0 +1,8 @@
+package com.popyoyo.kotlin.ktweb
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
